406354c0cd2da1398d6aca0b657de95d99030729
ThagFSD <thangnguyen1913@gmail.com>
2024-08-30 09:49:24 +0700

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Mongodb is connected");
+});
 
 app.listen(3000, () => {
-    console.log('Server is running at port 3000');
+  console.log("Server is running at port 3000");
 });
