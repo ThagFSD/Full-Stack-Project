ee4fc37754771505711f96152f79939de6e703e0
ThagFSD <Thangnguyen1913@gmail.com>
2024-08-30 12:02:48 +0700

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("Mongodb is connected");
 });
@@ -12,3 +16,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 app.listen(3000, () => {
   console.log("Server is running at port 3000");
 });
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
